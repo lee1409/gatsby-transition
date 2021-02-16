27d6fbdd9b9cb3992914b396beeb719b113558a7
lee1409 <leerun1409@gmail.com>
2021-02-16 22:50:15 +0800

    create black transition page

---
+++ Wrapper.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { createPortal } from "react-dom"
+
+import {WrapperContext} from './context';
+
+
+export default function Wrapper() {
+  const {on} = React.useContext(WrapperContext);
+
+  return createPortal(
+    <div
+      style={{
+        top: 0,
+        left: 0,
+        position: "absolute",
+        width: on ? "100vw" : 0,
+        height: "100vh",
+        backgroundColor: "black",
+      }}
+    ></div>,
+    document.body
+  )
+}
