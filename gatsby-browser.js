11534c7ab85158558896c79eccec005fb35202df
lee1409 <leerun1409@gmail.com>
2021-02-16 22:50:02 +0800

    add to wrapPageElement

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,16 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from "react"
+import Wrapper from "./Wrapper"
+import Provider from "./Provider"
+
+export const wrapPageElement = ({ element, props }) => {
+  return (
+    <Provider>
+      {element}
+      <Wrapper/>
+    </Provider>
+  )
+}
